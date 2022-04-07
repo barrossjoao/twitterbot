59e4c39085d7734ae7aac4e00bdba58c9a2a4404
barrossjoao <joao_barross@hotmail.com>
2022-04-07 19:29:45 -0300

    new features and hiding password from main

--- novobot.py
+++ novobot.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import time
 import random
 import io
+import keys
 
 #pegando a info do dolar
 while True:
@@ -16,8 +17,8 @@ while True:
 
 
     #bot twitter 
-    auth = tweepy.OAuthHandler('xIO5AGh0Y1vqczgQmUd2HcDzs', 'aC0HVV8jZ9pHHP5ttW4MkXTP4jIwggPjCnpUKy8ToA4fw8sQYu')
-    auth.set_access_token('1297448302682791936-YFZxif5aFoZ0Bg65GKkem4dhUk0We4', 'l3mL77m4cPneZajS0mthMvuKehnpJOW47uxucAgKn5hCZ')
+    auth = tweepy.OAuthHandler(keys.key, keys.secret)
+    auth.set_access_token(keys.token, keys.token_secret)
 
     api = tweepy.API(auth, wait_on_rate_limit=True)
 
